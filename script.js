e28733fd7e361fe1c732a9d0c8e9d6c0212f829a
anantharjun672001-a11y <anantharjun672001@gmail.com>
2025-11-20 14:11:15 +0530

    using function with array,loop,if,push method

--- script.js
+++ script.js
@@ -67,3 +67,32 @@ for(var i=0;i<arr.length;i++)
 {
     console.log(arr[i])
 }
+
+// function with array,loop,push,if methods
+
+var even=[];
+var odd=[];
+var invalid=[];
+
+function evenOrOdd(arr){
+    for(let i=0;i<arr.length;i++){
+        if(arr[i]<0){
+            invalid.push(arr[i]);
+        }
+        else if(arr[i]===0){
+            invalid.push(arr[i]);
+        }
+        else if(arr[i]%2===0){
+            even.push(arr[i]);
+        }
+        else{
+            odd.push(arr[i]);
+        }
+    }
+    return {
+        evenarray:even,
+        oddarray:odd,
+        invalidarray:invalid
+    }
+}
+console.log(evenOrOdd([1,2,3,4,5,0,-5,6,-7,8]))
