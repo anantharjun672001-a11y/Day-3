1c0ac4986c5860d20dc07deeb4b2a7d484832cf0
anantharjun672001-a11y <anantharjun672001@gmail.com>
2025-11-20 13:08:16 +0530

    Function with multiple returns

--- script.js
+++ script.js
@@ -10,11 +10,33 @@ function sub(a,b,c){
 sub(30,40,50)
 sub(10,2,3) */
 
-function add(a,b,c){
+/* function add(a,b,c){
     return a+b+c;
 }
 function sub(a,b,c){
     return a-b-c;
 }
 console.log(add(10,20,30))
-console.log(sub(10,2,2))
+console.log(sub(10,2,2)) */
+
+//Function with multiple returns
+//Type:1 Object
+
+function arithmetic(a,b){
+    return{        
+        sum:a+b,
+        sub: a-b,
+        mul: a*b,
+        div: a/b,
+        reminder: a%b
+    }
+
+}
+console.log(arithmetic(10,3))
+
+//Type:2 Array
+
+function arithmetic1(a,b){
+    return [a+b,a-b,a*b,a/b,a%b]
+}
+console.log(arithmetic1(10,3))
