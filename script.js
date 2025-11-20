b2f15d97883d29ed5083afdff12860e4162b3f4d
anantharjun672001-a11y <anantharjun672001@gmail.com>
2025-11-20 12:51:43 +0530

    javascript function

---
+++ script.js
@@ -0,0 +1,20 @@
+/* function add(a,b,c){
+    console.log(a+b+c)
+}
+add(10,10,30)
+add(50,10,40)
+
+function sub(a,b,c){
+    console.log(a-b-c)
+}
+sub(30,40,50)
+sub(10,2,3) */
+
+function add(a,b,c){
+    return a+b+c;
+}
+function sub(a,b,c){
+    return a-b-c;
+}
+console.log(add(10,20,30))
+console.log(sub(10,2,2))
